9d800326d403364ac19cfb9bac40bf47082689a8
Ankit Soni <akt_soni@yahoo.co.in>
2022-06-15 22:54:16 +0200

    carousel added
    dynamic carousel added and styled

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,8 +1,10 @@
 import { renderServices } from "./renderServices.js";
 import { renderPortfolio } from "./renderPortfolio.js";
 import { renderTeam } from "./renderTeam.js";
+import { renderTestimonies } from "./renderTestimonies.js";
 
 
 renderPortfolio();
 renderServices();
 renderTeam();
+renderTestimonies();
