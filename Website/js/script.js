714c981e894f0c5a855b2460bd871cd88e064b71
Ankit Soni <akt_soni@yahoo.co.in>
2022-06-17 23:42:00 +0200

    script updated with contact file

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,7 +1,7 @@
 import { renderServices } from "./renderServices.js";
 import { renderPortfolio } from "./renderPortfolio.js";
 import { renderTeam } from "./renderTeam.js";
-<<<<<<< HEAD
+import { submitForm } from "./submitForm.js";
 import { renderTestimonies } from "./renderTestimonies.js";
 import { scrollToTopBtn } from "./scrollToTopBtn.js";
 
@@ -10,11 +10,6 @@ renderPortfolio();
 renderServices();
 renderTeam();
 renderTestimonies();
-=======
-import { submitForm } from "./submitForm.js";
-
-renderPortfolio();
-renderServices();
-renderTeam();
 submitForm();
->>>>>>> 4601aa44813b46b00b260cd2e321791b57837f02
+
+
