4601aa44813b46b00b260cd2e321791b57837f02
alexrezaei <74854699+alexrezaei@users.noreply.github.com>
2022-06-17 21:51:08 +0200

    contact section done

--- Website/js/script.js
+++ Website/js/script.js
@@ -1,8 +1,9 @@
 import { renderServices } from "./renderServices.js";
 import { renderPortfolio } from "./renderPortfolio.js";
 import { renderTeam } from "./renderTeam.js";
-
+import { submitForm } from "./submitForm.js";
 
 renderPortfolio();
 renderServices();
 renderTeam();
+submitForm();
